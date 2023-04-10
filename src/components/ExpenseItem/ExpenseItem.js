0a78d9d5f5a8fe7072cb9e8d0fd5dea4030ad366
KotenkoNata <kotenkonata8@gmail.com>
2023-04-10 17:11:41 +0300

    feat: split components into multiple components, add style

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,13 +1,16 @@
 import './ExpenseItem.css';
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
 function ExpenseItem({amount, title, date}) {
 
-    return <div className='expense-item'>
-        <div>{date.toISOString()}</div>
+    return (
+        <div className='expense-item'>
+        <ExpenseDate date={date} />
         <div className='expense-item__description'>
             <h2>{title}</h2>
         </div>
         <div className='expense-item__price'>{amount}</div>
     </div>
+    )
 }
 
 export default ExpenseItem;
