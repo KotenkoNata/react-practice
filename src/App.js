29a5a96f9d1e15fbf7b24bbca469c4a53abac962
KotenkoNata <kotenkonata8@gmail.com>
2023-04-10 16:47:49 +0300

    feat: pass data via props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {
+            id: "e1",
+            title: "Toilet Paper",
+            amount: 94,
+            date: new Date(2023, 3, 8),
+        },
+        {
+            id: "e2",
+            title: "Car insurance",
+            amount: 294,
+            date: new Date(2023, 2, 2),
+        }
+    ]
+
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
+        {expenses.map(item=><ExpenseItem key={item.id}
+                                         amount={item.amount}
+                                         date={item.date}
+                                         title={item.title}/>)}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
+function ExpenseItem({amount, title, date}) {
+
     return <div className='expense-item'>
-        <div>March 28th 2023</div>
+        <div>{date.toISOString()}</div>
         <div className='expense-item__description'>
-            <h2>Car insurance</h2>
+            <h2>{title}</h2>
         </div>
-        <div className='expense-item__price'>$294</div>
+        <div className='expense-item__price'>{amount}</div>
     </div>
 }
 
