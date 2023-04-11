476c725916fdc57445551654ff17df142e00ada1
KotenkoNata <kotenkonata8@gmail.com>
2023-04-11 16:41:02 +0300

    ref: rewrite classes concatenations

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -9,7 +9,7 @@ function ExpenseItem({amount, title, date}) {
         <div className='expense-item__description'>
             <h2>{title}</h2>
         </div>
-        <div className='expense-item__price'>{amount}</div>
+        <div className='expense-item__price'>$ {amount}</div>
         </Card>
     )
 }
