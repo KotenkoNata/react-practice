2e84ad5185632fb856795f2748f0f954e1b0e9d3
KotenkoNata <kotenkonata8@gmail.com>
2023-04-12 16:55:31 +0300

    feat: add form input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
     const expenses = [
@@ -18,6 +19,7 @@ function App() {
 
   return (
     <div>
+        <NewExpense />
         <Expenses expenses = {expenses}/>
     </div>
   );
