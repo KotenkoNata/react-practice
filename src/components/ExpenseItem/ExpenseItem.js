e97d115a075c7a5333e55cc9064e411fe2644b41
KotenkoNata <kotenkonata8@gmail.com>
2023-04-13 20:51:06 +0300

    feat: create filter component

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -3,19 +3,15 @@ import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../Card/Card";
 import {useState} from "react";
 function ExpenseItem({amount, title, date}) {
-    const [updatedTitle, setUpdatedTitle] = useState(title);
-    const clickHandler = () => {
-        setUpdatedTitle("Clicked")
-    }
 
     return (
         <Card className='expense-item'>
         <ExpenseDate date={date} />
         <div className='expense-item__description'>
-            <h2>{updatedTitle}</h2>
+            <h2>{title}</h2>
         </div>
         <div className='expense-item__price'>$ {amount}</div>
-            <button onClick={clickHandler}> Change title </button>
+            <button> Change title </button>
         </Card>
     )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,17 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import './Expenses.css';
 import Card from "../Card/Card";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+import {useState} from "react";
 
 function Expenses({expenses}) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) =>{
+        setFilteredYear(selectedYear)
+    }
     return (
         <Card className="expenses">
+            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             {expenses.map(item=><ExpenseItem key={item.id}
                                              amount={item.amount}
                                              date={item.date}
