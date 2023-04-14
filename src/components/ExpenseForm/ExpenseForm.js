60f9a2d644bf0bde1cfbbfd3402a72e86a6cb16b
KotenkoNata <kotenkonata8@gmail.com>
2023-04-14 16:36:13 +0300

    ref: convert string to number submitHandler

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -30,7 +30,7 @@ const ExpenseForm = ({onSaveExpenseData}) => {
         e.preventDefault();
         const expenseData = {
             title: userInput.enteredTitle,
-            amount: userInput.enteredAmount,
+            amount: +userInput.enteredAmount,
             date: new Date(userInput.enteredDate)
         }
 
